ccf37252010f205dbbaf6b6ae3c7ec636a32b1cb
“pagadalasunilkumar” <pagadalasunilkumar111@gmail.com>
2023-05-07 11:55:24 +0530

    Add:  UC-2FLippingCoinMultipleTimes

--- flipcoinsimulator.sh
+++ flipcoinsimulator.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+declare -i heads_count=0
+declare -i tails_count=0
+
 flip_coin() {
     local result=$((RANDOM % 2))
 
     if ((result == 0)); then
         echo "Heads"
+        heads_count+=1
     else
         echo "Tails"
+        tails_count+=1
     fi
 }
 
-winner=$(flip_coin)
+read -p "Enter the number of times to flip the coin: " num_flips
 
-echo "The winner is: $winner"
+for ((i=1; i<=num_flips; i++)); do
+    result=$(flip_coin)
+    echo "Flip $i: $result"
+done
+
+echo "Heads count: $heads_count"
+echo "Tails count: $tails_count"
+
